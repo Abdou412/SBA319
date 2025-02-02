614497fc20e4192a95786e45f6cfc3d3c4b5c39c
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-02-01 23:23:13 -0500

    awsdfg

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-        
+      const express = require('express');
+        const app = express();
+        require('dotenv').config();
+        const PORT =  process.env.PORT
+        app.listen(PORT, () => {
+          console.log(`hey abdou do you know that the Server is running on port ${PORT}`);
+        });
