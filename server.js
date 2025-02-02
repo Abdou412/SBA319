6ad8a1694aeadde80ddf1f69f8eca136e5ca46ed
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2025-02-02 17:12:19 -0500

    add postes

--- server.js
+++ server.js
@@ -5,25 +5,43 @@ const { Book, User, Theater } = require("./schema");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri);
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`hi abdou, you are connected on ${mongoose.connection.name}!`);
 });
 
-app.get("/books", (req, res) => {
-  Book.find().then((books) => res.send(books));
-});
+app.use(express.json()); 
 
-app.get("/users", (req, res) => {
-  User.find().then((users) => res.send(users));
-});
 
-app.get("/theaters", (req, res) => {
-  Theater.find().then((theaters) => res.send(theaters));
-});
+app
+  .route("/books")
+  .get((req, res) => {
+    Book.find().then((books) => res.send(books));
+  })
+  .post((req, res) => {
+    new Book(req.body).save().then((book) => res.send(book));
+  });
+  
+
+app
+  .route("/users")
+  .get((req, res) => {
+    User.find().then((users) => res.send(users));
+  })
+  .post((req, res) => {
+    new User(req.body).save().then((user) => res.send(user));
+  });
+
+
+app
+  .route("/theaters")
+  .get((req, res) => {
+    Theater.find().then((theaters) => res.send(theaters));
+  })
+  .post((req, res) => {
+    new Theater(req.body).save().then((theater) => res.send(theater));
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on :${port}`);
